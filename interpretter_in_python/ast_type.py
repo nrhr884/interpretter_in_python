b49db1e0b7b864b48cbb8e7d0bd94859c473bbc2
Naruhiro Hayashi <naruhiro_hayashi@so-netmedia.jp>
2021-01-17 00:23:54 +0900

    add string

--- interpretter_in_python/ast_type.py
+++ interpretter_in_python/ast_type.py
@@ -8,6 +8,9 @@ class Node(ABC):
     @abstractclassmethod
     def token_literal(self) -> str:
         pass
+    @abstractclassmethod
+    def string(self) -> str:
+        pass
 
 
 @dataclass
@@ -33,32 +36,51 @@ class Program(Node):
     def token_literal(self):
         return self.statements[0].token_literal() if self.statements else ""
 
+    def string(self):
+        strings = []
+        for stmt in self.statements():
+            strings.append(stmt.string())
+        return "\n".join(strings)
+
+
 @dataclass
 class Identifier(Expression):
     value: str
+    def string(self):
+        return self.token.literal
 
 
 @dataclass
 class IntegerLiteral(Expression):
     value: int
+    def string(self):
+        return self.token.literal
 
 @dataclass
 class PrefixExpression(Expression):
     operator: str
     right: Expression
+    def string(self):
+        return f"({self.operator}{self.right.string()})"
 
 @dataclass
 class LetStatement(Statement):
     name: Identifier
     value: Expression
+    def string(self):
+        return f'{self.token_literal()} {self.name.string()} = {self.value.string()};'
 
 
 @dataclass
 class ReturnStatement(Statement):
     return_value: Expression
+    def string(self):
+        return f'{self.token_literal()} {self.return_value.string()};'
 
 
 @dataclass
 class ExpressionStatement(Statement):
     expression: Expression
+    def string(self):
+        return self.expression.string()
 
